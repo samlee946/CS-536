2eee2b73319157f3d53896cbf1e6da9be3f749ea
samlee946 <604657407@qq.com>
2019-03-11 23:50:45 -0400

    finished

--- Assignments/RegressionProblems/main.py
+++ Assignments/RegressionProblems/main.py
@@ -11,6 +11,10 @@ def question5(m = 200, w = 1, b = 5, sig_2 = 0.1):
     b_hat_list = []
     w_prime_hat_list = []
     b_prime_hat_list = []
+    w_the_hat_list = []
+    b_the_hat_list = []
+    w_prime_hat_the_list = []
+    b_prime_hat_the_list = []
     for i in range(repeat_times):
         sz = (1, m)
         x = np.random.uniform(100, 102, sz)
@@ -25,6 +29,15 @@ def question5(m = 200, w = 1, b = 5, sig_2 = 0.1):
         b_hat_list.append(b_hat)
         w_prime_hat_list.append(w_prime_hat)
         b_prime_hat_list.append(b_prime_hat)
+
+        w_the = w + np.sum((x - np.mean(x)) * eps) / m / np.var(x)
+        b_the = np.mean(y) - w_the * np.mean(x)
+        w_prime_the = w + np.sum((x_prime - np.mean(x_prime)) * eps) / m / np.var(x_prime)
+        b_prime_the = np.mean(y) - w_the * np.mean(x_prime)
+        w_the_hat_list.append(w_the)
+        w_prime_hat_the_list.append(w_prime_the)
+        b_the_hat_list.append(b_the)
+        b_prime_hat_the_list.append(b_prime_the)
     print('Expected values:')
     print('w_hat: {}'.format(np.mean(w_hat_list)))
     print('b_hat: {}'.format(np.mean(b_hat_list)))
@@ -35,6 +48,16 @@ def question5(m = 200, w = 1, b = 5, sig_2 = 0.1):
     print('b_hat: {}'.format(np.var(b_hat_list)))
     print('w_prime_hat: {}'.format(np.var(w_prime_hat_list)))
     print('b_prime_hat: {}'.format(np.var(b_prime_hat_list)))
+    print('Theoretical values:')
+    print('w_hat: {}'.format(np.mean(w_the_hat_list)))
+    print('b_hat: {}'.format(np.mean(b_the_hat_list)))
+    print('w_prime_hat: {}'.format(np.mean(w_prime_hat_the_list)))
+    print('b_prime_hat: {}'.format(np.mean(b_prime_hat_the_list)))
+    print('Theoretical variances:')
+    print('w_hat: {}'.format(np.var(w_the_hat_list)))
+    print('b_hat: {}'.format(np.var(b_the_hat_list)))
+    print('w_prime_hat: {}'.format(np.var(w_prime_hat_the_list)))
+    print('b_prime_hat: {}'.format(np.var(b_prime_hat_the_list)))
     return
 
 if __name__ == '__main__':
