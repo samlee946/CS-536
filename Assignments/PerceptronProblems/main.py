d606ca825c7091a00a8e8d44a1a3d0fac43d79c9
samlee946 <604657407@qq.com>
2019-03-28 18:13:02 -0400

    update

--- Assignments/PerceptronProblems/main.py
+++ Assignments/PerceptronProblems/main.py
@@ -2,23 +2,46 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.spatial import ConvexHull
 
-def perceptron(X, Y, max_epoch = 1000):
+def perceptron(X, Y, max_epoch = 1000, realtime_plot = False):
     m, k = X.shape
     w = np.zeros((k, 1))
     X[:, 0] = np.ones(m)
     steps = 0
-    for epoch in range(max_epoch):
+    w_history = np.zeros((max_epoch, 3))
+    w_history_2 = []
+    act_epoch = 0
+    epoch = 0
+    while epoch < max_epoch:
         flag = False
+        err_cnt = 0
         for i in range(m):
             f = 1 if np.matmul(X[i, :], w) > 0 else -1
             #print(i, Y[i], f)
             if f != Y[i]:
+                err_cnt += 1
                 steps += 1
                 flag = True
                 w = w + (Y[i] * X[i]).reshape(w.shape)
                 #print(w)
+                w_history[epoch] = w.reshape(3)
+                w_history_2.append(w)
+        act_epoch = max(act_epoch, epoch)
+        #print(err_cnt)
+        max_epoch = min(max_epoch, epoch + 2 * err_cnt)
+        epoch += 1
         if flag == False:
             break
+    if realtime_plot == True:
+        fig, ax = plt.subplots()
+        plt.xlabel('epoch')
+        plt.ylabel('value')
+        line_b, = ax.plot(w_history[:, 0], '-b', label='b')
+        line_w1, = ax.plot(w_history[:, 1], '-r', label='w_1')
+        line_w2, = ax.plot(w_history[:, 2], '-k', label='w_2')
+        #line_t, = ax.plot(list(T0), err_T, '--ro', label='Error_test')
+        ax.legend()
+        plt.show()
+
     gamma = 1e9
     w_norm = np.linalg.norm(w)
     #gamma_t = 1e9
@@ -26,7 +49,7 @@ def perceptron(X, Y, max_epoch = 1000):
     #tw[k - 1] = 1
     #tw_norm = np.linalg.norm(tw)
     for i in range(m):
-        #print(i, Y[i], np.matmul(X[i, :], tw))
+        #print(i, Y[i], np.matmul(X[i, :], w))
         gamma = min(gamma, np.abs(np.matmul(X[i, :], w)) / w_norm)
         #gamma_t = min(gamma_t, np.abs(np.matmul(X[i, :], tw)) / tw_norm)
     #print(tw, gamma, gamma_t)
@@ -131,23 +154,24 @@ def question4(save = False, save_file = 'q4.png'):
 
 def question5(save = False, save_file = 'q5.png'):
     X, Y = get_data_q5(k = 2, m = 100, load = True)
-    fig, ax = plt.subplots()
-    plt.xlabel('x_1')
-    plt.ylabel('x_2')
-    idx_neg = []
-    idx_pos = []
-    for i in range(X.shape[0]):
-        if Y[i] > 0:
-            idx_pos.append(i)
-        else:
-            idx_neg.append(i)
-    ax.plot(X[idx_neg, 1], X[idx_neg, 2], 'ro', label = 'Y = -1')
-    ax.plot(X[idx_pos, 1], X[idx_pos, 2], 'bo', label = 'Y = 1')
-    ax.legend()
-    if save == False:
-        plt.show()
-    else:
-        plt.savefig(save_file)
+    w, steps = perceptron(X, Y, max_epoch = 500, realtime_plot = True)
+    #fig, ax = plt.subplots()
+    #plt.xlabel('x_1')
+    #plt.ylabel('x_2')
+    #idx_neg = []
+    #idx_pos = []
+    #for i in range(X.shape[0]):
+    #    if Y[i] > 0:
+    #        idx_pos.append(i)
+    #    else:
+    #        idx_neg.append(i)
+    #ax.plot(X[idx_neg, 1], X[idx_neg, 2], 'ro', label = 'Y = -1')
+    #ax.plot(X[idx_pos, 1], X[idx_pos, 2], 'bo', label = 'Y = 1')
+    #ax.legend()
+    #if save == False:
+    #    plt.show()
+    #else:
+    #    plt.savefig(save_file)
 
 if __name__ == '__main__':
     #question3(save = True)
